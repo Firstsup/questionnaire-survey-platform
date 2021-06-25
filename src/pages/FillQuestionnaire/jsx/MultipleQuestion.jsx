e4198081a8d86c329988eda207e34f56ad432d75
Firstsup <45390884+Firstsup@users.noreply.github.com>
2021-06-25 20:13:11 +0800

    修改css

--- src/pages/FillQuestionnaire/jsx/MultipleQuestion.jsx
+++ src/pages/FillQuestionnaire/jsx/MultipleQuestion.jsx
@@ -26,21 +26,21 @@ class MultipleQuestion extends Component {
         let questionID = this.props.questionID;
         return (
             <div className={"question"}>
-                <div className={"left"}>
+                <div className={"question_left"}>
                     {
-                        question.isNecessary ? (<p className={"isNecessary"}>*</p>) : null
+                        question.isNecessary ? (<p className={"question_isNecessary"}>*</p>) : null
                     }
                 </div>
                 <Title className={"question_title"}
                        level={4}>{questionID + 1}.&nbsp;{question.subject}&nbsp;<span className={"question_span"}>[多选题]</span>&nbsp;&nbsp;{this.state.warringVisible[questionID] ? (
-                    <span className={"show_warring"}>请填写本题</span>) : null}</Title>
-                <div className={"left"}/>
+                    <span className={"question_show_warring"}>请填写本题</span>) : null}</Title>
+                <div className={"question_left"}/>
                 <Checkbox.Group onChange={this.handleMultipleChange}>
                     <Space direction="vertical">
                         {
                             question.options.map((option, optionID) => {
                                 return (
-                                    <Checkbox className={"checkbox"} key={optionID} value={optionID + 1}
+                                    <Checkbox key={optionID} value={optionID + 1}
                                     >{option}</Checkbox>
                                 )
                             })
