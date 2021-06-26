8e519f93d3d8523b2ab0ed631abdfa33e68f2329
Firstsup <45390884+Firstsup@users.noreply.github.com>
2021-06-26 21:45:56 +0800

    更新App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 import QuestionnaireSubmitComplete from "./pages/FillQuestionnaire/jsx/QuestionnaireSubmitComplete";
 import TempTestDetail from "./pages/ViewQuestionnaireDetail/jsx/TempTestDetail";
 import ShowAllQuestionnaire from "./pages/ManageQuestionnaire/jsx/ShowAllQuestionnaire"
+import DataAnalysis from "./pages/DataAnalysis/jsx/DataAnalysis";
 
 class App extends Component {
     render() {
@@ -13,6 +14,7 @@ class App extends Component {
                 <Route path={'/fillquestionnaire'} component={FillQuestionnaire}/>
                 <Route path={'/fillquestionnairesubmitcomplete'} component={QuestionnaireSubmitComplete}/>
                 <Route path={'/temptestdetail'} component={TempTestDetail}/>
+                <Route path={'/dataanalysis'} component={DataAnalysis}/>
                 <Route path={'/'} exact component={ShowAllQuestionnaire}/>
             </BrowserRouter>
         )
