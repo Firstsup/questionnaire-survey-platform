45280d9c82852443efffdfcb955cff54d44eac86
Firstsup <45390884+Firstsup@users.noreply.github.com>
2021-07-10 11:34:49 +0800

    更新时间转换函数

--- src/utils/TimeConversion.js
+++ src/utils/TimeConversion.js
@@ -3,8 +3,8 @@ const timeConversion = (timestamp) => {
     const Y = date.getFullYear() + '-';
     const M = date.getMonth() + 1 + '-';
     const D = date.getDate() + ' ';
-    const h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours() + ':';
-    const m = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes() + ':';
+    const h = date.getHours() < 10 ? '0' + date.getHours() + ":" : date.getHours() + ':';
+    const m = date.getMinutes() < 10 ? '0' + date.getMinutes() + ":" : date.getMinutes() + ':';
     const s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
     return Y + M + D + h + m + s;
 }
